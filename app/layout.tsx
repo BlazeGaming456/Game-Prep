9d42a4fd5fc993f86676271f8e640667c968e08c
unknown <ajincraju@gmail.com>
2025-06-19 02:51:20 +0530

    Added the Home Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,26 @@
-import type { Metadata } from "next";
-import { Mona_Sans } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Mona_Sans } from 'next/font/google'
+import './globals.css'
 
 const monaSans = Mona_Sans({
-  variable: "--font-mona-sans",
-  subsets: ["latin"],
-});
+  variable: '--font-mona-sans',
+  subsets: ['latin']
+})
 
 export const metadata: Metadata = {
-  title: "Create Next App",
-  description: "An AI-powered platform to help perpare YOU prepare for interviews!",
-};
+  title: 'Create Next App',
+  description:
+    'An AI-powered platform to help perpare YOU prepare for interviews!'
+}
 
-export default function RootLayout({
-  children,
+export default function RootLayout ({
+  children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className="dark">
-      <body
-        className={`${monaSans.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang='en' className='dark'>
+      <body className={`${monaSans.variable} antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
